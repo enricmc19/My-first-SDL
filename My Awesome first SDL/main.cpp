8caabdc1c2f9ecc03024515e8e6e8c44368700f2
enricmc19 <enricmc19@users.noreply.github.com>
2020-02-21 09:53:02 +0100

    Changes in first class

--- My Awesome first SDL/main.cpp
+++ My Awesome first SDL/main.cpp
@@ -1,8 +1,27 @@
+#include "SDL/include/SDL.h"
+#pragma comment( lib, "SDL/lib/x86/SDL2.lib" )
+#pragma comment( lib, "SDL/lib/x86/SDL2main.lib" )
+#include <iostream>
 
 
 
-
-
-int main(void) {
+int main(int argc, char** argv) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
 	return 0;
-}
+	}
